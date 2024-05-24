d74e2e4fe6d9645e45fdc34c7c8f9e22c38fc439
KindaZhang <2930767060@qq.com>
2024-05-24 12:08:29 +0800

    hello

---
+++ biginner/variable_type_02/hello_world.py
@@ -0,0 +1,14 @@
+#hello
+
+print("Hello Python world!")
+
+
+message = 'Hello Python world!'
+print(message)
+message = "Hello Python Crash Course world!"
+print(message)
+
+#变量名规则
+#数字、下划线、字母组成，数字不能打头，不能有空格
+#关键字和函数名不能作为变量名
+#慎用i和O，易混淆
